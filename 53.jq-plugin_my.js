e2f76bb3ec3c24c231d9706d3af89efe96daeb74
jiang490094 <jfr08100810@gmail.com>
2020-09-18 17:00:24 +0800

    0918重新上傳

---
+++ 53.jq-plugin_my.js
@@ -0,0 +1,9 @@
+// $.fn.eason = function(){
+//     $(this).css("color","red");
+//     return $(this);
+// }
+
+$.fn.jiang = function(){
+    $(this).css("background-color","lightblue");
+    return $(this);
+}
